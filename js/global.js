d804a37e5a23147ec49c2cae8cd9e4fc93aae208
hoonine2 <hoonine2@gmail.com>
2021-02-26 18:38:16 +0900

    mobile
    active효과

--- js/global.js
+++ js/global.js
@@ -12,3 +12,17 @@ $(document).ready(function() {
         }); 
     });
 });
+
+
+
+
+$(document).ready(function() {
+    $('.btn').live('touchstart',function(event){
+         $(this).addClass('active');
+     });
+     $('.btn').live('touchend',function(event){
+         $(this).removeClass('active');
+     });
+ });
+ 
+ 
